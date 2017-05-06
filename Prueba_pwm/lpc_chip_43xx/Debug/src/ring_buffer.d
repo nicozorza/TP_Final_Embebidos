e0cba3b0471c23c9382e076779f884141651da9c
nicozorza <nico.zorzano@live.com.ar>
2017-05-06 17:48:26 -0300

    Empece a probar el pwm. Simplemente importe el ejemplo que hay, pero creo que sin osciloscopio no voy a poder ver bien como funciona. Voy a probar midiendo el valor medio.

---
+++ Prueba_pwm/lpc_chip_43xx/Debug/src/ring_buffer.d
@@ -0,0 +1,7 @@
+src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_pwm/lpc_chip_43xx/inc/ring_buffer.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_pwm/lpc_chip_43xx/inc/lpc_types.h
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_pwm/lpc_chip_43xx/inc/ring_buffer.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_pwm/lpc_chip_43xx/inc/lpc_types.h:
