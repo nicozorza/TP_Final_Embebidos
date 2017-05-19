c62163386adbd6ecab03d956085df719feeeff52
nicozorza <nico.zorzano@live.com.ar>
2017-05-19 15:47:43 -0300

    Arme los circuitos de deteccion de fase y disparo del tiristor. Cree un proyecto (Prueba_detectar_fase) donde activo las interrupcione de entrada de un pin (para detectar los cruces), meto un delay, y luego disparo el gate del tiristor. Funciono perfectamente pero esta todo hecho re cabeza. Tendria que emprolijarlo y empezar a ver de pasarlo a FreeRTOS.

---
+++ Prueba_detectar_fase/lpc_chip_43xx/Debug/src/fpu_init.d
@@ -0,0 +1,28 @@
+src/fpu_init.o src/fpu_init.d: ../src/fpu_init.c \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/sys_config.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/cmsis.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/lpc_types.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/sys_config.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/cmsis_43xx.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cm4.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cmInstr.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cmFunc.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cm4_simd.h
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/sys_config.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/cmsis.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/lpc_types.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/sys_config.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/cmsis_43xx.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cm4.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cmInstr.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cmFunc.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/core_cm4_simd.h:
