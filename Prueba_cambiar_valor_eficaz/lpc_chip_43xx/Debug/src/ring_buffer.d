98874de795e1a6fb3f62630c71477c0e2babfc7d
nicozorza <nico.zorzano@live.com.ar>
2017-05-21 19:44:08 -0300

    Arme el proyecto Prueba_cambiar_valor_eficaz, donde lo que hago es con un pote variar la tension de un canal del adc, y en funcion de esa tension cambio el valor eficaz de la altera. Para esto lo que hice fue usar un timer para hacer el delay, y lo voy regulando de forma lineal (era indespejable la expresion posta). Funciono todo bien, pero sigue siendo medio cabeza el codigo

---
+++ Prueba_cambiar_valor_eficaz/lpc_chip_43xx/Debug/src/ring_buffer.d
@@ -0,0 +1,7 @@
+src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_cambiar_valor_eficaz/lpc_chip_43xx/inc/ring_buffer.h \
+ /home/nicozorza/LPCXpresso/TP_Final/Prueba_cambiar_valor_eficaz/lpc_chip_43xx/inc/lpc_types.h
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_cambiar_valor_eficaz/lpc_chip_43xx/inc/ring_buffer.h:
+
+/home/nicozorza/LPCXpresso/TP_Final/Prueba_cambiar_valor_eficaz/lpc_chip_43xx/inc/lpc_types.h:
