402bd6413bf7b99aa05b61f15cfd18e2aeb1f708
nicozorza <nico.zorzano@live.com.ar>
2017-05-22 11:25:00 -0300

    Reorganice el proyecto un poco porque estaba todo re tirado

--- Prueba_detectar_fase/lpc_chip_43xx/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/ring_buffer.h \
- /home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/lpc_types.h
-
-/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/ring_buffer.h:
-
-/home/nicozorza/LPCXpresso/TP_Final/Prueba_detectar_fase/lpc_chip_43xx/inc/lpc_types.h:
